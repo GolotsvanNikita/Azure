8365976548621650eefde067a8592c31378c6a4a
NikitaGolotsvan <hohoko555888@gmail.com>
2025-12-18 14:46:59 +0200

    Initial commit

---
+++ AzureP33/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using AzureP33.Models;
+using AzureP33.Models.Home;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AzureP33.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(HomeIndexFormModel? formModel)
+        {
+            HomeIndexViewModel viewModel = new()
+            {
+                PageTitle = "Translation",
+                FormModel = formModel?.Action == null ? null : formModel,
+            };
+
+            if (formModel?.Action != null) 
+            {
+                viewModel.FormModel = formModel;
+
+                if (string.IsNullOrWhiteSpace(formModel.OriginalText))
+                {
+                    viewModel.ErrorMessage = "Please enter any text to translate";
+                }
+                else 
+                {
+                    /////
+                }
+            }
+
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureP33/Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AzureP33.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public String PageTitle { get; set; } = "";
+
+        public HomeIndexFormModel? FormModel { get; set; }
+
+        public String? ErrorMessage { get; set; }
+
+        //public static implicit operator HomeIndexViewModel?(HomeIndexFormModel? v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
